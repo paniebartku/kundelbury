f3e4a1c34ce5c9db7b963597a5fc2460320eb24e
Bartłomiej Wilczek <bartlomiejwilczek@o2.pl>
2019-12-12 17:40:45 +0100

    no width, but max-width for images, 404, blog fixs

--- 404.php
+++ 404.php
@@ -1,4 +1,28 @@
 <?php 
-header("HTTP/1.1 301 Moved Permanently");
-header("Location: ".get_bloginfo('url'));
-exit();
+
+get_header();
+?>
+<section class="block-page">
+    <div class="container-fluid orange-row">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="block-page__title ">
+                        <h1>Taka strona nie istnieje :(</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container block-page__content">
+        <div class="row">
+            <div class="col-12">
+            <h1>Przykro nam, ale strona o takim adresie nie istnieje</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+get_footer();
+?>
